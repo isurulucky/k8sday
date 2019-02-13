1b69cb9652c855b37039aa65f5daa99653a414c3
Deependra Ariyadewa <deependra@gmail.com>
2019-02-13 17:27:31 +0530

    Add vdi download.

--- 00-setup/kday_create_vm.sh
+++ 00-setup/kday_create_vm.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+#Create folder
+mkdir ~/kday
 #Download "K-Day 01" VirtualBox image (vdi).
-#Create a folder named "K-Day 01" and copy the K-Day 01.vdi to the folder.
-
+wget http://atuwa.private.wso2.com/VMs/K-Day%2001.vdi.tgz -P ~/kday
 #Create VirtualBox entry.
 VBoxManage createvm --name "K-Day 01" --ostype Ubuntu_64 --register
 #Create ssh port forwarding.
@@ -9,6 +10,6 @@ VBoxManage modifyvm "K-Day 01" --ostype Ubuntu_64 --cpus 2 --memory 4000 --natpf
 #Create the sata controller.
 VBoxManage storagectl "K-Day 01"  --name hd1 --add sata --portcount 2
 #Point the downloaded ubuntu vdi file using media switch.
-VboxManage storageattach "K-Day 01" --storagectl hd1 --port 1 --type hdd --medium  "/Users/deep/VirtualBox VMs/K-Day 01/K-Day 01.vdi" --setuuid ""
+VboxManage storageattach "K-Day 01" --storagectl hd1 --port 1 --type hdd --medium  "~/kday/K-Day 01.vdi" --setuuid ""
 #Start the vm
 VBoxManage startvm "K-Day 01"
