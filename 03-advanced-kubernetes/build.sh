e37a3059c486ea9ae5f81201459152ab14c8b388
RameshaKaru <ramesha@wso2.com>
2019-04-30 11:24:43 +0530

    Set build image env variables for linux

--- 03-advanced-kubernetes/build.sh
+++ 03-advanced-kubernetes/build.sh
@@ -4,7 +4,7 @@ export CGO_ENABLED=0
 DOCKER_REPO=<your_dockerhub_id>
 DOCKER_TAG=v1.1.0
 
-go build -o hello-controller -x ./cmd/controller
+GOOS=linux GOARCH=amd64 go build -o hello-controller -x ./cmd/controller
 
 docker build -t ${DOCKER_REPO}/k8s-hello-controller:${DOCKER_TAG} .
 docker push ${DOCKER_REPO}/k8s-hello-controller:${DOCKER_TAG}
